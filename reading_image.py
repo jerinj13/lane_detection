b727ff5d2f16046a68c33dda664fed2c3cf36df2
jerinj13 <125982656+jerinj13@users.noreply.github.com>
2023-02-21 17:36:18 +0400

    Input path of file

--- reading_image.py
+++ reading_image.py
@@ -1,13 +1,21 @@
 import cv2
 
 # Load an image from a file
-img = cv2.imread('path_of_image_file')
+img = cv2.imread('lane_detection\solidWhiteRight.jpg')
+
+# if to be resized, else comment
+# set new width and height
+new_width = 640
+new_height = 480
+
+# resize image
+resized_img = cv2.resize(img, (new_width, new_height))
 
 # Display the image
-cv2.imshow('image', img)
+cv2.imshow('image', resized_img)
 
 # Wait for a key press to close the window
-cv2.waitKey(1)
+cv2.waitKey(0)
 
 # Release resources
 cv2.destroyAllWindows()
