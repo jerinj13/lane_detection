ea5596147110db992054592e2c132c69ebdf8268
jerinj13 <125982656+jerinj13@users.noreply.github.com>
2023-02-21 17:20:54 +0400

    Reading video in OpenCV

---
+++ reading_video.py
@@ -0,0 +1,28 @@
+import cv2
+
+# create a VideoCapture object
+cap = cv2.VideoCapture('path_of_video_file')
+
+# check if the video was successfully opened
+if not cap.isOpened():
+    print("Error opening video file")
+
+# loop through the frames of the video
+while cap.isOpened():
+    # read the next frame
+    ret, frame = cap.read()
+
+    # check if there was an error reading the frame
+    if not ret:
+        break
+
+    # display the frame
+    cv2.imshow('Video', frame)
+
+    # wait for a key event to exit
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# release the VideoCapture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
